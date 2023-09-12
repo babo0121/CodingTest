08257c4a10a00607dbd2fba11310e8d2c027fa2d
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-09-12 10:12:23 +0900

    [Silver II] Title: 생태학, Time: 1284 ms, Memory: 122764 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -0,0 +1,39 @@
+// 생태학
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        Map<String, Double> map = new TreeMap<>();
+
+        String str = "";
+        Double count;
+        Double totalCount = 0.0;
+        while(true) {
+            str = br.readLine();
+            if(str == null || str.length() == 0)
+                break;
+
+            if(map.containsKey(str)) {    // 이미 값이 존재할 경우
+                count = map.get(str) + 1;
+                map.put(str, count);
+            }
+            else {  // 존재하지 않을 경우
+                map.put(str, 1.0);
+            }
+            totalCount++;
+        }
+        for(Map.Entry<String, Double> entry: map.entrySet()) {
+            System.out.print(entry.getKey());
+            System.out.println(" " + String.format("%.4f", entry.getValue() / totalCount * 100));
+        }
+
+    }
+}
